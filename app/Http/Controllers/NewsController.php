09d687a3452c33c02764f0f8c5bd25e3cdf059d7
chet21 <free_mans@i.ua>
2020-09-16 15:26:11 +0300

    save

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Comment;
+use Egulias\EmailValidator\Exception\ExpectingAT;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
@@ -15,24 +16,28 @@ class NewsController extends Controller
     public function oneArticle(Request $request)
     {
         if(preg_match('/^[0-9]+$/', $request->ident)){
-            $old_article = DB::connection('old_server')->table('news')->where('id', intval($request->ident))->first();
-            $category = new \stdClass();
-            $category->title_ua = '1';
+           try{
+               $old_article = DB::connection('old_server')->table('news')->where('id', intval($request->ident))->first();
+               $category = new \stdClass();
+               $category->title_ua = '1';
 
-            $article = new \stdClass();
-            $article->id = $old_article->id;
-            $article->category_id = $old_article->id_category;
-            $article->origin_id = $old_article->id_donor;
-            $article->title_ua = $old_article->title_ua;
-            $article->title_ru = $old_article->title_ru;
-            $article->text_ua = $old_article->text_ua;
-            $article->text_ru = $old_article->text_ru;
-            $article->img = $old_article->img;
-            $article->views = $old_article->view ?? 0;
-            $article->active = 1;
-            $article->created_at = $old_article->time;
+               $article = new \stdClass();
+               $article->id = $old_article->id;
+               $article->category_id = $old_article->id_category;
+               $article->origin_id = $old_article->id_donor;
+               $article->title_ua = $old_article->title_ua;
+               $article->title_ru = $old_article->title_ru;
+               $article->text_ua = $old_article->text_ua;
+               $article->text_ru = $old_article->text_ru;
+               $article->img = $old_article->img;
+               $article->views = $old_article->view ?? 0;
+               $article->active = 1;
+               $article->created_at = $old_article->time;
 
-            $article->categories = $category;
+               $article->categories = $category;
+           }catch (\Exception $e){
+               abort(404);
+           }
         }else{
             $request = explode('_n', $request->ident);
             $article = Article::with(['tags', 'comments', 'origin'])->where(['id' => $request[1], 'slug' => $request[0]])->first();
