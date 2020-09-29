e5f908e7e1015148da8c4c064e62ba510c9c88f3
Serhii Babych <serhii.babych@gmail.com>
2020-09-29 20:58:13 +0300

    select menu oly with full category news

--- resources/views/module/menu.blade.php
+++ resources/views/module/menu.blade.php
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="row">
             <a style="color: #7BCEF8; font-family: 'PT Sans Narrow', sans-serif; padding: 13px 15px 0px 13px; text-decoration: none" href="/">Головна</a>
-            @foreach(\App\Models\Category::all()->sortBy('position') as $category)
+            @foreach(\App\Models\Category::whereHas('articles', function ($query) {
+                    $query->where('img', '!=', '');
+                }, '>', 8)->get()->sortBy('position') as $category)
                 <span style="font-family: 'PT Sans Narrow', sans-serif; padding: 13px 15px 0px 13px">
                     <a style="color: white; " href="/category/{{ $category->slug }}">{{ $category->title_ua }}</a>
                 </span>
